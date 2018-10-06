12fdbdefcd6075b3434089dd4bfcafe81aa4a309
Jordan <woovie@woovie.net>
2018-10-06 12:43:15 -0400

    Add baseURI variable, add /favicon 404

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import gambling_subprocessor as gambler
 import configparser, os, json
 
+baseURI = '/'
+
 welcomeString = """welcome to rolldie.live!
 to use rolldie.live, simply pass your dice roll as such:
   rolldie.live/1d20
@@ -10,17 +12,14 @@ source code on https://github.com/woovie/rolldie.live"""
 def application(env, start_response):
     returnString = ''
     reqURI = env['REQUEST_URI']
-    if reqURI == '/rolldie':
+    if 'favicon' in reqURI:
+        start_response('404 File Not Found')
+        return [b'404 File Not Found']
+    if reqURI == baseURI:
         returnString = welcomeString
-    elif reqURI.startswith('/rolldie/') and len(reqURI) > 11:
-        quantity, sides = reqURI.replace('/rolldie/', '').split('d')
+    elif reqURI.startswith(baseURI) and len(reqURI) > 3:
+        quantity, sides = reqURI.replace(baseURI, '').split('d')
         returnString = ", ".join(str(x) for x in gambler.simple_math(int(quantity), int(sides)))
     else:
         returnString = f"{welcomeString}\n\nSorry, it seems the provided URI is invalid:\n{reqURI}"
     return [f"{returnString}\n".encode()]
-    
-
-#    if message.content.startswith('!roll'):
-#            "{0}: {1}".format(message.author, gambler.roll(message.content)))
-#    elif message.content.startswith('!cast'):
-#            "{0}: {1}".format(message.author, gambler.cast(message.content)))
