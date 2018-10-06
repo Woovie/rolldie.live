61dab8fae608ca0b1ad7f46b58136d1f649dc784
Jordan <woovie@woovie.net>
2018-10-06 11:58:40 -0400

    Get basic rolls working, add uwsgi.ini

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import gambling_subprocessor as gambler
-import configparser
-import os
+import configparser, os, json
+
+welcomeString = """welcome to rolldie.live!
+to use rolldie.live, simply pass your dice roll as such:
+  rolldie.live/1d20
+
+source code on https://github.com/woovie/rolldie.live"""
 
 def application(env, start_response):
-    start_response('2OO OK', [('Content-Type', 'plaintext')])
-    return [b'welcome to rolldie.live!']
+    returnString = ''
+    reqURI = env['REQUEST_URI']
+    if reqURI == '/rolldie':
+        returnString = welcomeString
+    elif reqURI.startswith('/rolldie/') and len(reqURI) > 11:
+        quantity, sides = reqURI.replace('/rolldie/', '').split('d')
+        returnString = ", ".join(str(x) for x in gambler.simple_math(int(quantity), int(sides)))
+    else:
+        returnString = f"{welcomeString}\n\nSorry, it seems the provided URI is invalid:\n{reqURI}"
+    return [f"{returnString}\n".encode()]
     
 
-#    # Roll dice, example: !roll 1d20 or !roll 1d20+3.
 #    if message.content.startswith('!roll'):
-#        await client.send_message(
-#            message.channel,
 #            "{0}: {1}".format(message.author, gambler.roll(message.content)))
-#    # Stereotypical help message. Needs work.
-#    elif message.content.startswith('!help'):
-#        await client.send_message(
-#            message.channel,
-#            '{0}: Please use !roll 1d20 to roll.'.format(message.author))
-#    # Uses json file to perform specific rolls based on the string following
-#    # !cast
 #    elif message.content.startswith('!cast'):
-#        await client.send_message(
-#            message.channel,
 #            "{0}: {1}".format(message.author, gambler.cast(message.content)))
-
-client.run(token)
