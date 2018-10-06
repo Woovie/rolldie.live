479bac46851a8593a0dac11c75118f2bc4e8123b
Jordan <woovie@woovie.net>
2018-10-06 13:20:51 -0400

    Added json, csv, and raw output, added total output, added removal of 'rolldie/' for simplicity sake

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import gambling_subprocessor as gambler
-import configparser, os, json
+import configparser, json, re
 
+baseURIRemove = 'rolldie/'
 baseURI = '/'
+formats = ['json', 'csv', 'raw']
 
 welcomeString = """welcome to rolldie.live!
 to use rolldie.live, simply pass your dice roll as such:
@@ -11,15 +13,32 @@ source code on https://github.com/woovie/rolldie.live"""
 
 def application(env, start_response):
     returnString = ''
-    reqURI = env['REQUEST_URI']
+    reqURI = env['REQUEST_URI'].replace(baseURIRemove, '')
     if 'favicon' in reqURI:
         start_response('404 File Not Found')
         return [b'404 File Not Found']
+    reqSplit = reqURI.split('/')
     if reqURI == baseURI:
         returnString = welcomeString
-    elif reqURI.startswith(baseURI) and len(reqURI) > 3:
-        quantity, sides = reqURI.replace(baseURI, '').split('d')
-        returnString = ", ".join(str(x) for x in gambler.simple_math(int(quantity), int(sides)))
+    elif re.match("^\d+d\d+$", reqSplit[1]):
+        quantity, sides = reqSplit[1].split('d')
+        gamblerResult = gambler.simple_math(int(quantity), int(sides))
+        if len(reqSplit) == 2:
+            returnString = ", ".join(str(x) for x in gamblerResult)
+            if len(gamblerResult) > 1:
+                returnString += f"\nTotal: {sum(gamblerResult)}"
+        if len(reqSplit) == 3:
+            if reqSplit[2] == 'json':
+                returnString = json.dumps(gamblerResult)
+            elif reqSplit[2] == 'csv':
+                returnString = ",".join(str(x) for x in gamblerResult)
+            elif reqSplit[2] == 'raw':
+                returnString = " ".join(str(x) for x in gamblerResult)
+            else:
+                returnString = ", ".join(str(x) for x in gamblerResult)
+                if len(gamblerResult) > 1:
+                    returnString += f"\nTotal: {sum(gamblerResult)}"
+                returnString += f"\n\nProvided format type ({reqSplit[2]}) incompatible, available options: {', '.join(formats)}"
     else:
         returnString = f"{welcomeString}\n\nSorry, it seems the provided URI is invalid:\n{reqURI}"
     return [f"{returnString}\n".encode()]
